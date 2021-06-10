905fd5bacc0551c376151e9f5b73230a98d6e180
myemail4ik <58517192+myemail4ik@users.noreply.github.com>
2021-06-10 20:51:33 +0300

    Delete btc/btc directory

--- btc/btc/install_modules.bat
+++
@@ -1,7 +0,0 @@
-pip freeze > requirements.txt && pip uninstall -r requirements.txt -y
-pip install mnemonic
-pip install bip32utils
-pip install requests
-pip install colored
-pip install notify-py
-pip install pysocks
